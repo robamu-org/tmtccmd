7a6a277cbbfdc232f82141459adb084efc15546a
Robin Mueller <robin.mueller.m@gmail.com>
2021-03-15 23:41:29 +0100

    everything moved to setup.cfg

---
+++ core/__init__.py
@@ -0,0 +1,3 @@
+from version import SW_VERSION, SW_SUBVERSION
+
+__version__ = f"{SW_VERSION}.{SW_SUBVERSION}"
--- setup.py
+++ setup.py
@@ -1,15 +1,4 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+import setuptools
 
-from tmtccmd.core.version import SW_VERSION, SW_SUBVERSION
 
-setup(
-    version=f"{SW_VERSION},{SW_SUBVERSION}",
-    install_requires=[
-        'crcmod>=1.7',
-        'PyQt5>=5.0',
-        'pyserial>=3.0'
-    ],
-)
+setuptools.setup()
