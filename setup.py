2e3c642963e2a8c6a43cd7618280e69646f6139f
Robin Mueller <robin.mueller.m@gmail.com>
2021-03-16 03:06:03 +0100

    trying some more stuff

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 try:
-    from setuptools import setup
+    from setuptools import setup, find_packages
 except ImportError:
     from distutils.core import setup
 
 
 setup(
+    packages=["tmtccmd.core", ],
 )
