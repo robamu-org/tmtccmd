3e339c6d4149bc6a3f20f36b73fafbd60b762659
Robin Mueller <robin.mueller.m@gmail.com>
2021-03-16 12:06:47 +0100

    pyQt is otpinal now

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ try:
 except ImportError:
     from distutils.core import setup
 
-
+# We do the package handling in the setup.py so we can have
 setup(
-    package_dir={"": "src"},
-    packages=find_packages(where="src"),
+    # package_dir={"": "src"},
+    # packages=find_packages(where="src"),
 )
