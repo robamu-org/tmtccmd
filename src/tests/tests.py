62765a581a38ad00b5969a72a6a6c7028c69ad5d
Robin Mueller <robin.mueller.m@gmail.com>
2021-03-16 12:36:54 +0100

    added test stub

---
+++ src/tests/tests.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+import tmtccmd
+
+class TestTelemetry(TestCase):
+    def test_stub(self):
+        self.assertTrue(True)
+
+class TestTelecommand(TestCase):
+    def test_stub(self):
+        self.assertTrue(True)
