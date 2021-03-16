9124fda05abc8da847cb1b0d43a723de54bf6a85
Robin Mueller <robin.mueller.m@gmail.com>
2021-03-16 12:47:14 +0100

    test stubs added and working

--- src/tests/tests.py
+++ src/tests/tests.py
@@ -1,11 +1,15 @@
+import unittest
 from unittest import TestCase
 
 import tmtccmd
 
 class TestTelemetry(TestCase):
-    def test_stub(self):
+    def test_tm(self):
         self.assertTrue(True)
 
 class TestTelecommand(TestCase):
-    def test_stub(self):
+    def test_tc(self):
         self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
