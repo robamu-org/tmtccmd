7573b0e34264a4194fc44fba94b5e3e9efa8ca6d
Robin Mueller <robin.mueller.m@gmail.com>
2021-03-19 16:24:11 +0100

    added way to add custom mode iterables
    or custom service iterables

--- src/tmtccmd/defaults/globals_setup.py
+++ src/tmtccmd/defaults/globals_setup.py
@@ -167,11 +167,34 @@ def check_args_in_enum(param: any, int_enum: collections.Iterable,
     return True, param
 
 
-def check_and_set_core_mode_arg(mode_arg: any):
+def check_and_set_core_mode_arg(mode_arg: any, custom_mode_int_enum: collections.Iterable = None):
+    """
+    Checks whether the mode argument is contained inside the core mode list integer enumeration
+    or a custom mode list integer enumeraration which can be passed optionally.
+    This function will set the single command mode as the global mode parameter if the passed mode
+    is not found in either enumerations.
+    :param mode_arg:
+    :param custom_mode_int_enum:
+    :return:
+    """
     in_enum, mode_value = check_args_in_enum(
         param=mode_arg, int_enum=CoreModeList, warning_hint="mode"
     )
-    if not in_enum:
+    if in_enum:
+        update_global(CoreGlobalIds.MODE, mode_value)
+        return
+
+    mode_arg_invalid = False
+    if custom_mode_int_enum is not None:
+        in_enum, mode_value = check_args_in_enum(
+            param=mode_arg, int_enum=custom_mode_int_enum, warning_hint="custom mode"
+        )
+        if not in_enum:
+            mode_arg_invalid = True
+    else:
+        mode_arg_invalid = True
+
+    if mode_arg_invalid:
         LOGGER.warning(f"Passed mode argument might be invalid, "
                        f"setting to {CoreModeList.SINGLE_CMD_MODE}")
         mode_value = CoreModeList.SINGLE_CMD_MODE
@@ -189,11 +212,27 @@ def check_and_set_core_com_if_arg(com_if_arg: any):
     update_global(CoreGlobalIds.COM_IF, com_if_value)
 
 
-def check_and_set_core_service_arg(service_arg: any):
+def check_and_set_core_service_arg(
+        service_arg: any, custom_service_list: collections.Iterable = None
+):
     in_enum, service_value = check_args_in_enum(
         param=service_arg, int_enum=CoreServiceList, warning_hint="service"
     )
-    if not in_enum:
+    if in_enum:
+        update_global(CoreGlobalIds.CURRENT_SERVICE, service_value)
+        return
+
+    service_arg_invalid = False
+    if custom_service_list is not None:
+        in_enum, mode_value = check_args_in_enum(
+            param=service_arg, int_enum=custom_service_list, warning_hint="custom mode"
+        )
+        if not in_enum:
+            service_arg_invalid = True
+    else:
+        service_arg_invalid = True
+
+    if service_arg_invalid:
         LOGGER.warning(f"Passed service argument might be invalid, "
                        f"setting to {CoreServiceList.SERVICE_17}")
         service_value = CoreServiceList.SERVICE_17
