c0a2c35af645a0050f7d2dee66bd59ff27486735
Robin Mueller <robin.mueller.m@gmail.com>
2021-05-04 18:13:26 +0200

    refactored object id manager

--- src/tmtccmd/core/object_id_manager.py
+++ src/tmtccmd/core/object_id_manager.py
@@ -27,7 +27,7 @@ class ObjectIdManager:
         self.object_id_dict = dict()
 
     # noinspection PyUnresolvedReferences
-    def get_object_id(self, object_id_key: int):
+    def get_object_id_info(self, object_id: bytearray):
         object_id = self.object_id_dict.get(object_id_key)
         if object_id is None:
             try:
@@ -38,47 +38,27 @@ class ObjectIdManager:
         else:
             return object_id
 
-    def get_key_from_raw_object_id(self, object_id: bytearray):
-        for key, raw_id in self.object_id_dict.items():
-            if raw_id == object_id:
-                return key
-        return None
+    def get_object_id_info_from_int_id(self, object_id: int):
+        object_id_bytearray = struct.pack('!I', object_id)
+        return get_object_id_info(object_id=object_id_bytearray)
 
-    def insert_object_id(self, object_id_key: int, object_id: bytearray):
+    def insert_object_id(self, object_id: bytearray, object_id_info: list):
         self.object_id_dict.update({object_id_key: object_id})
 
-    def insert_object_ids(self, object_id_dict: Dict[int, bytearray]):
+    def insert_object_ids(self, object_id_dict: Dict[bytearray, list]):
         self.object_id_dict.update(object_id_dict)
 
 
-def insert_object_id(object_id_key: int, object_id: bytearray):
+def insert_object_id(object_id: bytearray, object_id_info: list):
     return ObjectIdManager.get_manager().insert_object_id(
-        object_id_key=object_id_key, object_id=object_id
+        object_id=object_id, object_id_info=object_id_info
     )
 
 
-def insert_object_ids(object_id_dict: Dict[int, bytearray]):
+def insert_object_ids(object_id_dict: Dict[bytearray, list]):
     if object_id_dict is not None:
         return ObjectIdManager.get_manager().insert_object_ids(object_id_dict=object_id_dict)
 
 
-def get_object_id(object_id_key: int):
-    return ObjectIdManager.get_manager().get_object_id(object_id_key)
-
-
-def get_key_from_raw_object_id(object_id_raw: bytearray) -> int:
-    if not isinstance(object_id_raw, bytearray):
-        LOGGER.warning("Invalid object ID type.")
-        return CoreObjectIds.INVALID
-    if len(object_id_raw) != 4:
-        LOGGER.warning("Invalid object ID length")
-        return CoreObjectIds.INVALID
-    return ObjectIdManager.get_manager().get_key_from_raw_object_id(object_id_raw)
-
-
-def get_key_from_int_object_id(object_id_int: int) -> int:
-    if not isinstance(object_id_int, int):
-        LOGGER.warning("Invalid object ID type.")
-        return CoreObjectIds.INVALID
-    object_id_raw = bytearray(struct.pack("!I", object_id_int))
-    return ObjectIdManager.get_manager().get_key_from_raw_object_id(object_id_raw)
+def get_object_id_info(object_id: bytearray):
+    return ObjectIdManager.get_manager().get_object_id(object_id)
