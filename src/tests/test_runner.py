1bd9fa354b0233c81b5a431d1ca8d8c6243301d0
Robin Mueller <robin.mueller.m@gmail.com>
2021-06-18 16:42:58 +0200

    fixed tests

--- src/tests/test_runner.py
+++ src/tests/test_runner.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from tmtccmd.ccsds.handler import CcsdsTmHandler
 from tmtccmd.runner import run_tmtc_commander, initialize_tmtc_commander, get_default_tmtc_backend
 from tests.backend_mock import create_backend_mock, create_hook_mock, create_frontend_mock
 
@@ -7,7 +8,8 @@ class TestTmtcRunner(TestCase):
 
     def test_tmtc_runner(self):
         hook_base = create_hook_mock()
-        backend_mock = create_backend_mock()
+        tm_handler = CcsdsTmHandler(tmtc_printer=None)
+        backend_mock = create_backend_mock(tm_handler=tm_handler)
         initialize_tmtc_commander(hook_object=hook_base)
         run_tmtc_commander(False, False, True, tmtc_backend=backend_mock)
         backend_mock.start_listener.assert_called_with()
@@ -20,7 +22,9 @@ class TestTmtcRunner(TestCase):
         frontend_mock.start.assert_called_once()
         qt_app = frontend_mock.start.call_args[0][0]
         self.assertTrue(qt_app is None)
-        default_backend = get_default_tmtc_backend(hook_obj=hook_base, json_cfg_path="tmtc_config.json")
+        default_backend = get_default_tmtc_backend(
+            hook_obj=hook_base, tm_handler=tm_handler, json_cfg_path="tmtc_config.json"
+        )
         self.assertTrue(default_backend is not None)
 
     def test_errors(self):
