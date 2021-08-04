caf0fdf566ab5fad1b88dffc6b1409d078e43ca5
Robin Mueller <robin.mueller.m@gmail.com>
2021-08-04 16:01:14 +0200

    added eof PDU

--- src/tmtccmd/cfdp/pdu/eof.py
+++ src/tmtccmd/cfdp/pdu/eof.py
@@ -1,6 +1,27 @@
-from .file_directive import FileDirectivePduBase
+from tmtccmd.cfdp.pdu.file_directive import FileDirectivePduBase, DirectiveCodes
 
 
-class EofPdu(FileDirectivePduBase):
-    def __init__(self):
+class EofPdu():
+    def __init__(
+        self,
+        # PDU Header parameters
+        direction: Direction,
+        trans_mode: TransmissionModes,
+        crc_flag: CrcFlag,
+        len_entity_id: LenInBytes = LenInBytes.NONE,
+        len_transaction_seq_num=LenInBytes.NONE,
+    ):
+        self.pdu_file_directive = FileDirectivePduBase(
+            directive_code=DirectiveCodes.EOF_PDU,
+            direction=direction,
+            trans_mode=trans_mode,
+            crc_flag=crc_flag,
+            len_entity_id=len_entity_id,
+            len_transaction_seq_num=len_transaction_seq_num
+        )
+
+    def pack(self):
         pass
+
+    def unpack(self, raw_bytes: bytearray):
+        self.pdu_file_directive.unpack(raw_bytes=raw_bytes)
