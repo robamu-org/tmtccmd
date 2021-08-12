0b4af25773d0733d9aaed8e9ad02a89fd959f6fe
Robin Mueller <robin.mueller.m@gmail.com>
2021-08-12 20:27:03 +0200

    typo

--- src/tmtccmd/utility/dle_encoder.py
+++ src/tmtccmd/utility/dle_encoder.py
@@ -1,8 +1,8 @@
 """DLE Encoder Implementation
 DLE encoding can be used to provide a simple transport layer for serial data.
 A give data stream is encoded by adding a STX char at the beginning and an ETX char at the end.
-All STX and ETX occurences in the packet are encoded as well so the receiver can simply look
-for STX and ETX occurences to identify packets.
+All STX and ETX occurrences in the packet are encoded as well so the receiver can simply look
+for STX and ETX occurrences to identify packets.
 """
 import enum
 from typing import Tuple
