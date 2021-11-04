2c42ebb841169850b5bcece0f9774fe3b5224b92
Robin Mueller <robin.mueller.m@gmail.com>
2021-11-04 14:30:45 +0100

    should fix some errors

--- src/tmtccmd/tm/service_20_parameters.py
+++ src/tmtccmd/tm/service_20_parameters.py
@@ -42,13 +42,9 @@ class Service20TM(PusTmInfoBase, PusTmBase):
             secondary_header_flag: bool = True, space_time_ref: int = 0b0000,
             destination_id: int = 0
     ):
-        tm_data = object_id
-        self.object_id = ObjectId.from_bytes(obj_id_as_bytes=object_id)
-
         pus_tm = PusTelemetry(
             service=PusServices.SERVICE_20_PARAMETER,
             subservice=subservice_id,
-            source_data=tm_data,
             time=time,
             ssc=ssc,
             source_data=source_data,
@@ -60,7 +56,7 @@ class Service20TM(PusTmInfoBase, PusTmBase):
             destination_id=destination_id
         )
 
-
+        self.object_id = ObjectId.from_bytes(obj_id_as_bytes=object_id)
         self.param_struct = ParamStruct()
         self.param_struct.param_id = param_id
         self.param_struct.domain_id = domain_id
@@ -74,19 +70,14 @@ class Service20TM(PusTmInfoBase, PusTmBase):
     @staticmethod
     def __init_without_base(instance: Service20TM):
         tm_data = instance.tm_data
+        if len(tm_data) < 8:
+            return
         data_size = len(tm_data)
-        if data_size < 4:
-            LOGGER.warning("Invalid data length, less than 4")
-            return
-        elif data_size < 8:
-            LOGGER.warning("Invalid data length, less than 8 (Object ID and Parameter ID)")
-            return
-        else:
-            instance.object_id = ObjectId.from_bytes(obj_id_as_bytes=tm_data[0:4])
-            instance.param_struct.param_id = struct.unpack('!I', tm_data[4:8])[0]
-            instance.param_struct.domain_id = tm_data[4]
-            instance.param_struct.unique_id = tm_data[5]
-            instance.param_struct.linear_index = tm_data[6] << 8 | tm_data[7]
+        instance.object_id = ObjectId.from_bytes(obj_id_as_bytes=tm_data[0:4])
+        instance.param_struct.param_id = struct.unpack('!I', tm_data[4:8])[0]
+        instance.param_struct.domain_id = tm_data[4]
+        instance.param_struct.unique_id = tm_data[5]
+        instance.param_struct.linear_index = tm_data[6] << 8 | tm_data[7]
 
         if instance.subservice == 130:
             # TODO: This needs to be more generic. Furthermore, we need to be able to handle
@@ -103,10 +94,10 @@ class Service20TM(PusTmInfoBase, PusTmBase):
                     instance.param_struct.param = struct.unpack('!i', tm_data[12:16])[0]
                 if instance.param_struct.type_ptc == 5 and instance.param_struct.type_pfc == 1:
                     instance.param_struct.param = struct.unpack('!f', tm_data[12:16])[0]
-        else:
-            LOGGER.info(
-                "Error when receiving Pus Service 20 TM: subservice is not 130"
-            )
+            else:
+                LOGGER.info(
+                    "Error when receiving Pus Service 20 TM: subservice is not 130"
+                )
 
     @classmethod
     def __empty(cls) -> Service20TM:
@@ -127,6 +118,10 @@ class Service20TM(PusTmInfoBase, PusTmBase):
         service_20_tm.pus_tm = PusTelemetry.unpack(
             raw_telemetry=raw_telemetry, pus_version=pus_version
         )
+        if len(service_20_tm.pus_tm.tm_data) < 4:
+            LOGGER.warning("Invalid data length, less than 4")
+        elif len(service_20_tm.pus_tm.tm_data) < 8:
+            LOGGER.warning("Invalid data length, less than 8 (Object ID and Parameter ID)")
         service_20_tm.__init_without_base(instance=service_20_tm)
         return service_20_tm
 
