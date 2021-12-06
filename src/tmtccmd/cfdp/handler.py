f061e71d40db6bcf195f2c5d44bc5f79b5c2a6f5
Robin Mueller <robin.mueller.m@gmail.com>
2021-12-06 17:36:31 +0100

    added init cfdp handler

---
+++ src/tmtccmd/cfdp/handler.py
@@ -0,0 +1,9 @@
+from tmtccmd.utility.tmtc_printer import TmTcPrinter
+
+
+class CfdpHandler:
+    def __init__(self):
+        pass
+
+    def pass_packet(self, apid: int, raw_tm_packet: bytearray, tmtc_printer: TmTcPrinter):
+        pass
