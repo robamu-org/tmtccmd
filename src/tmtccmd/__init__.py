3a0d4d31df109595359351f7c74dd813e20250ca
Robin Mueller <robin.mueller.m@gmail.com>
2022-03-07 10:58:06 +0100

    bump minor version

--- src/tmtccmd/__init__.py
+++ src/tmtccmd/__init__.py
@@ -1,8 +1,8 @@
 VERSION_NAME = "tmtccmd"
 VERSION_MAJOR = 1
-VERSION_MINOR = 13
-VERSION_REVISION = 1
+VERSION_MINOR = 14
+VERSION_REVISION = 0
 
 # I think this needs to be in string representation to be parsed so we can't
 # use a formatted string here.
-__version__ = "1.13.1"
+__version__ = "1.14.0"
