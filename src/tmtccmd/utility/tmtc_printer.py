a31723786f4d85e82ba8e685f1652cb59cdb707b
Robin Mueller <robin.mueller.m@gmail.com>
2022-03-14 10:04:09 +0100

    auto-formatting

--- src/tmtccmd/utility/tmtc_printer.py
+++ src/tmtccmd/utility/tmtc_printer.py
@@ -595,4 +595,4 @@ class TmTcPrinter:
     def chunks(lst: List, n) -> List[List]:
         """Yield successive n-sized chunks from lst."""
         for i in range(0, len(lst), n):
-            yield lst[i:i + n]
+            yield lst[i : i + n]
