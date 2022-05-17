1a7a622718f4a2c605704a51e75cdc069414c129
Ulrich Mohr <mohr@irs.uni-stuttgart.de>
2022-05-17 16:28:19 +0200

    some regressions and debugging code from previous PRs

--- src/tmtccmd/sendreceive/sequential_sender_receiver.py
+++ src/tmtccmd/sendreceive/sequential_sender_receiver.py
@@ -102,7 +102,6 @@ class SequentialCommandSenderReceiver(CommandSenderReceiver):
             time.sleep(0.2)
 
     def __handle_tc_sending(self):
-        print("handle_tc_sending: " + str(self._tc_queue))
         while not self.__all_replies_received:
             while not self._tc_queue.__len__() == 0:
                 self.__check_for_reply()
@@ -120,9 +119,7 @@ class SequentialCommandSenderReceiver(CommandSenderReceiver):
             time.sleep(0.2)
         self._tm_listener.set_mode_op_finished()
         LOGGER.info("SequentialSenderReceiver: All replies received!")
-        while(True):
-            self.__check_for_reply()
-            time.sleep(0.2)
+
 
     def __check_for_reply(self):
         if self._tm_listener.reply_event():
