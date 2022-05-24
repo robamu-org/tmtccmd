f7db5221b82da2162441bbaa6f6cd46ad95e5e64
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2022-05-24 14:27:55 +0200

    add some generic TC sched cmd generation

--- src/tmtccmd/tc/pus_11_tc_sched.py
+++ src/tmtccmd/tc/pus_11_tc_sched.py
@@ -1,12 +1,36 @@
 from spacepackets.ecss import PusTelecommand
+from spacepackets.ecss.conf import FETCH_GLOBAL_APID
 from tmtccmd.pus.definitions import PusServices
 from tmtccmd.pus.pus_11_tc_sched import Subservices
 
 
-def generate_enable_tc_sched_cmd(ssc: int, apid: int = -1) -> PusTelecommand:
+def __generic_param_less_tc_sched_cmd(
+    subservice: int, ssc: int, apid: int = -1
+) -> PusTelecommand:
     return PusTelecommand(
-        service=PusServices.S11_TC_SCHED,
-        subservice=Subservices.TC_ENABLE,
-        ssc=ssc,
-        apid=apid
+        service=PusServices.S11_TC_SCHED, subservice=subservice, ssc=ssc, apid=apid
+    )
+
+
+def generate_enable_tc_sched_cmd(
+    ssc: int, apid: int = FETCH_GLOBAL_APID
+) -> PusTelecommand:
+    return __generic_param_less_tc_sched_cmd(
+        subservice=Subservices.TC_ENABLE, ssc=ssc, apid=apid
+    )
+
+
+def generate_disable_tc_sched_cmd(
+    ssc: int, apid: int = FETCH_GLOBAL_APID
+) -> PusTelecommand:
+    return __generic_param_less_tc_sched_cmd(
+        subservice=Subservices.TC_DISABLE, ssc=ssc, apid=apid
+    )
+
+
+def generate_reset_tc_sched_cmd(
+    ssc: int, apid: int = FETCH_GLOBAL_APID
+) -> PusTelecommand:
+    return __generic_param_less_tc_sched_cmd(
+        subservice=Subservices.TC_RESET, ssc=ssc, apid=apid
     )
