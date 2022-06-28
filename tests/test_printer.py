0ee8a169aad982f25c571657634f8f453056bd1c
Robin Mueller <robin.mueller.m@gmail.com>
2022-06-28 14:24:52 +0200

    bump ci/cd version and test on windows as well

--- tests/test_printer.py
+++ tests/test_printer.py
@@ -17,23 +17,18 @@ from tmtccmd.logging.pus import (
 )
 
 
-# TODO: Use temp files to test loggers
+# TODO: Use temp files to test loggers?gitas
 class TestPrintersLoggers(TestCase):
     def setUp(self):
         self.log_path = Path(LOG_DIR)
         if not self.log_path.exists():
             self.log_path.mkdir()
-        self.temp_file = TemporaryFile()
-        self.regular_tmtc_logger = RegularTmtcLogWrapper(self.temp_file)
-        # self.raw_tmtc_timed_log = RawTmtcTimedLogWrapper(
-        #    when=TimedLogWhen.PER_SECOND,
-        #    interval=1
-        # )
-        self.raw_tmtc_log = RawTmtcRotatingLogWrapper(max_bytes=1024, backup_count=10)
-        # self.tmtc_printer = FsfwTmTcPrinter(file_logger=self.regular_tmtc_logger.logger)
+        self.regular_file_name = Path(RegularTmtcLogWrapper.get_current_tmtc_file_name())
         self.logger = get_console_logger()
 
     def test_pus_loggers(self):
+        self.regular_tmtc_logger = RegularTmtcLogWrapper(self.regular_file_name)
+        self.raw_tmtc_log = RawTmtcRotatingLogWrapper(max_bytes=2048, backup_count=10)
         pus_tc = pack_service_17_ping_command(ssc=0)
         self.raw_tmtc_log.log_tc(pus_tc)
         pus_tm = Service1TmExtended(
@@ -42,7 +37,7 @@ class TestPrintersLoggers(TestCase):
             tc_request_id=RequestId(pus_tc.packet_id, pus_tc.packet_seq_ctrl),
         )
         self.raw_tmtc_log.log_tm(pus_tm.pus_tm)
-        self.assertTrue(Path(self.temp_file).exists())
+        self.assertTrue(Path(self.regular_file_name).exists())
         # There should be 2 files now because 1024 bytes are not enough to accomate all info
         self.assertTrue(Path(self.raw_tmtc_log.file_name).exists())
         self.assertTrue(Path(f"{self.raw_tmtc_log.file_name}.1").exists())
@@ -52,3 +47,5 @@ class TestPrintersLoggers(TestCase):
 
     def tearDown(self) -> None:
         """Reset the hook object"""
+        if self.regular_file_name.exists():
+            os.remove(self.regular_file_name)
