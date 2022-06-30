c8f19c63ac03490f6180299b8923b6fae87102d4
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2022-06-30 18:05:50 +0200

    finished verification logger

---
+++ tests/test_pus_verif_log.py
@@ -0,0 +1,67 @@
+from unittest import TestCase
+
+from spacepackets.ecss import PusTelecommand
+from spacepackets.ecss.pus_1_verification import (
+    create_acceptance_success_tm,
+    create_start_success_tm,
+    create_step_success_tm,
+    StepId,
+    create_completion_success_tm,
+    create_acceptance_failure_tm,
+    FailureNotice,
+    ErrorCode,
+    create_start_failure_tm,
+)
+from spacepackets.ecss.pus_verificator import PusVerificator
+from tmtccmd import get_console_logger
+from tmtccmd.pus import VerificationWrapper
+
+
+class TestPusVerifLog(TestCase):
+    def test_console_log_success(self):
+        logger = get_console_logger()
+        wrapper = VerificationWrapper(logger)
+        verificator = wrapper.verificator
+        tc = PusTelecommand(service=17, subservice=1, seq_count=0)
+        verificator.add_tc(tc)
+        srv_1_tm = create_acceptance_success_tm(tc)
+        res = verificator.add_tm(srv_1_tm)
+        wrapper.log_to_console(srv_1_tm, res)
+        srv_1_tm = create_start_success_tm(tc)
+        res = verificator.add_tm(srv_1_tm)
+        wrapper.log_to_console(srv_1_tm, res)
+        srv_1_tm = create_step_success_tm(tc, StepId.with_byte_size(1, 1))
+        res = verificator.add_tm(srv_1_tm)
+        wrapper.log_to_console(srv_1_tm, res)
+        srv_1_tm = create_completion_success_tm(tc)
+        res = verificator.add_tm(srv_1_tm)
+        wrapper.log_to_console(srv_1_tm, res)
+
+    def test_console_log_acc_failure(self):
+        logger = get_console_logger()
+        wrapper = VerificationWrapper(logger)
+        verificator = wrapper.verificator
+        tc = PusTelecommand(service=17, subservice=1, seq_count=1)
+        verificator.add_tc(tc)
+        srv_1_tm = create_acceptance_failure_tm(
+            tc, failure_notice=FailureNotice(code=ErrorCode(pfc=8, val=1), data=bytes())
+        )
+        res = verificator.add_tm(srv_1_tm)
+        wrapper.log_to_console(srv_1_tm, res)
+
+    def test_console_log_start_failure(self):
+        logger = get_console_logger()
+        wrapper = VerificationWrapper(logger)
+        verificator = wrapper.verificator
+        tc = PusTelecommand(service=17, subservice=1, seq_count=2)
+        verificator.add_tc(tc)
+        srv_1_tm = create_acceptance_failure_tm(
+            tc, failure_notice=FailureNotice(code=ErrorCode(pfc=8, val=1), data=bytes())
+        )
+        res = verificator.add_tm(srv_1_tm)
+        wrapper.log_to_console(srv_1_tm, res)
+        srv_1_tm = create_start_failure_tm(
+            tc, failure_notice=FailureNotice(code=ErrorCode(pfc=8, val=1), data=bytes())
+        )
+        res = verificator.add_tm(srv_1_tm)
+        wrapper.log_to_console(srv_1_tm, res)
--- tmtccmd/pus/__init__.py
+++ tmtccmd/pus/__init__.py
@@ -1,7 +1,119 @@
 from enum import IntEnum
+from typing import Optional
+
+from spacepackets.ecss import PusTelecommand
 from spacepackets.ecss.defs import PusServices
+from spacepackets.ecss.pus_1_verification import RequestId
+import spacepackets.ecss.pus_1_verification as pus_1
+from spacepackets.ecss.pus_verificator import (
+    VerificationStatus,
+    StatusField,
+    PusVerificator,
+    TmCheckResult,
+)
 from .seqcnt import FileSeqCountProvider, ProvidesSeqCount
+import logging
+
+from tmtccmd.utility.conf_util import AnsiColors
 
 
 class CustomPusServices(IntEnum):
     SERVICE_200_MODE = 200
+
+
+class VerificationWrapper:
+    def __init__(self, logger: logging.Logger):
+        self.pus_verificator = PusVerificator()
+        self.logger = logger
+        self.with_colors = True
+
+    @property
+    def verificator(self) -> PusVerificator:
+        return self.pus_verificator
+
+    def add_tc(self, pus_tc: PusTelecommand) -> bool:
+        return self.pus_verificator.add_tc(pus_tc)
+
+    def log_to_console(self, srv_1_tm: pus_1.Service1Tm, res: TmCheckResult):
+        self.log_to_console_from_req_id(srv_1_tm.tc_req_id, res, srv_1_tm.subservice)
+
+    def log_to_console_from_req_id(
+        self,
+        req_id: RequestId,
+        res: TmCheckResult,
+        subservice: Optional[pus_1.Subservices] = None,
+    ):
+        return self.log_progress_to_console_from_status(res.status, req_id, subservice)
+
+    def log_progress_to_console_from_status(
+        self,
+        status: VerificationStatus,
+        req_id: RequestId,
+        subservice: Optional[pus_1.Subservices] = None,
+    ):
+        acc_char = gen_char_from_status(status.accepted, self.with_colors)
+        start_char = gen_char_from_status(status.started, self.with_colors)
+        step_char = gen_char_from_status(status.step, self.with_colors)
+        fin_char = gen_char_from_status(status.completed, self.with_colors)
+        if not status.step_list:
+            step_num = 0
+        else:
+            step_num = max(status.step_list)
+        status_str = "Status"
+        if subservice is not None:
+            if subservice == pus_1.Subservices.TM_ACCEPTANCE_SUCCESS:
+                status_str = "Acceptance success"
+            elif subservice == pus_1.Subservices.TM_ACCEPTANCE_FAILURE:
+                status_str = "Acceptance failure"
+            elif subservice == pus_1.Subservices.TM_START_SUCCESS:
+                status_str = "Start success"
+            elif subservice == pus_1.Subservices.TM_START_FAILURE:
+                status_str = "Start failure"
+            elif subservice == pus_1.Subservices.TM_STEP_SUCCESS:
+                status_str = "Step success"
+            elif subservice == pus_1.Subservices.TM_STEP_FAILURE:
+                status_str = "Step failure"
+            elif subservice == pus_1.Subservices.TM_COMPLETION_SUCCESS:
+                status_str = "Completion success"
+            elif subservice == pus_1.Subservices.TM_COMPLETION_FAILURE:
+                status_str = "Completion failure"
+        first_str = f"{status_str} of TC".ljust(25)
+        second_str = f"Request ID {req_id.as_u32():#04x}"
+        completion_str = ""
+        if status.completed == StatusField.SUCCESS:
+            completion_str = f" \U0001F31F"
+        third_str = (
+            f"acc ({acc_char}) sta ({start_char}) ste ({step_char}, {step_num}) "
+            f"fin ({fin_char}){completion_str}"
+        )
+        self.logger.info(f"{first_str} | {second_str} | {third_str}")
+
+
+def gen_char_from_status(status: StatusField, with_color: bool):
+    if status == StatusField.UNSET:
+        return dash_unicode(with_color)
+    elif status == StatusField.SUCCESS:
+        return tick_mark_unicode(with_color)
+    elif status == StatusField.FAILURE:
+        return cross_mark_unicode(with_color)
+
+
+def dash_unicode(with_color: bool) -> str:
+    if with_color:
+        return f"{AnsiColors.YELLOW}-{AnsiColors.RESET}"
+    else:
+        return "-"
+
+
+def tick_mark_unicode(with_color: bool) -> str:
+    if with_color:
+        return f"{AnsiColors.GREEN}\u2713{AnsiColors.RESET}"
+    else:
+        return "\u2713"
+
+
+def cross_mark_unicode(with_color: bool) -> str:
+    if with_color:
+        return f"{AnsiColors.RED}\u274c{AnsiColors.RESET}"
+    else:
+        return "\u274c"
