ad23d29824aaa852164f471d674163096bb30596
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2022-06-30 16:41:38 +0200

    some tests do not work on windows

--- tests/test_seq_cnt_provider.py
+++ tests/test_seq_cnt_provider.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import platform
 from unittest import TestCase
 
 from tmtccmd.pus.seqcnt import FileSeqCountProvider
@@ -11,19 +12,20 @@ class TestSeqCount(TestCase):
         self.file_name = Path("seq_cnt.txt")
 
     def test_basic(self):
-        with NamedTemporaryFile("w+t") as file:
-            file.write("0\n")
-            file.seek(0)
-            seq_cnt_provider = FileSeqCountProvider(Path(file.name))
-            seq_cnt = seq_cnt_provider.current()
-            self.assertEqual(seq_cnt, 0)
-            self.assertEqual(next(seq_cnt_provider), 1)
-            self.assertEqual(seq_cnt_provider.next_seq_count(), 2)
-            file.seek(0)
-            file.write(f"{pow(2, 14) - 1}\n")
-            file.flush()
-            # Assert rollover
-            self.assertEqual(next(seq_cnt_provider), 0)
+        if platform.system() != "Windows":
+            with NamedTemporaryFile("w+t") as file:
+                file.write("0\n")
+                file.seek(0)
+                seq_cnt_provider = FileSeqCountProvider(Path(file.name))
+                seq_cnt = seq_cnt_provider.current()
+                self.assertEqual(seq_cnt, 0)
+                self.assertEqual(next(seq_cnt_provider), 1)
+                self.assertEqual(seq_cnt_provider.next_seq_count(), 2)
+                file.seek(0)
+                file.write(f"{pow(2, 14) - 1}\n")
+                file.flush()
+                # Assert rollover
+                self.assertEqual(next(seq_cnt_provider), 0)
 
     def test_with_real_file(self):
         seq_cnt_provider = FileSeqCountProvider(self.file_name)
@@ -42,18 +44,19 @@ class TestSeqCount(TestCase):
             seq_cnt_provider.current()
 
     def test_faulty_file_entry(self):
-        with NamedTemporaryFile("w+t") as file:
-            file.write("-1\n")
-            file.seek(0)
-            seq_cnt_provider = FileSeqCountProvider(Path(file.name))
-            with self.assertRaises(ValueError):
-                next(seq_cnt_provider)
-            file.write(f"{pow(2, 15)}\n")
-            file.seek(0)
-            file.flush()
-            seq_cnt_provider = FileSeqCountProvider(Path(file.name))
-            with self.assertRaises(ValueError):
-                next(seq_cnt_provider)
+        if platform.system() != "Windows":
+            with NamedTemporaryFile("w+t") as file:
+                file.write("-1\n")
+                file.seek(0)
+                seq_cnt_provider = FileSeqCountProvider(Path(file.name))
+                with self.assertRaises(ValueError):
+                    next(seq_cnt_provider)
+                file.write(f"{pow(2, 15)}\n")
+                file.seek(0)
+                file.flush()
+                seq_cnt_provider = FileSeqCountProvider(Path(file.name))
+                with self.assertRaises(ValueError):
+                    next(seq_cnt_provider)
 
     def tearDown(self) -> None:
         if self.file_name.exists():
