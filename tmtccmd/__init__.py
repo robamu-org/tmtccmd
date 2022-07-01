36ec2363b2a8bf6074591f3d014bdef909e4bb80
Robin Mueller <robin.mueller.m@gmail.com>
2022-07-01 11:44:02 +0200

    prepare v3.0.0rc1

--- tmtccmd/__init__.py
+++ tmtccmd/__init__.py
@@ -23,7 +23,7 @@ VERSION_REVISION = 0
 
 # I think this needs to be in string representation to be parsed so we can't
 # use a formatted string here.
-__version__ = "3.0.0"
+__version__ = "3.0.0rc1"
 
 
 LOGGER = get_console_logger()
