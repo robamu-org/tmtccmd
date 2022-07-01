e1359ab86eac3762a4fd6ce3623aee0526b41150
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2022-07-01 17:00:09 +0200

    add some more backend tests

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -14,7 +14,7 @@ from tmtccmd.tc import (
     DefaultProcedureInfo,
     TcProcedureType,
     ProcedureCastWrapper,
-    TcQueueEntryBase,
+    TcQueueEntryBase, PacketCastWrapper,
 )
 from tmtccmd.tc.ccsds_seq_sender import SenderMode
 from tmtccmd.tc.handler import FeedWrapper
@@ -25,9 +25,15 @@ class TcHandlerMock(TcHandlerBase):
         super().__init__()
         self.is_feed_cb_valid = False
         self.feed_cb_call_count = 0
+        self.feed_cb_def_proc_count = 0
+        self.send_cb_call_count = 0
+        self.send_cb_call_args = None
+        self.send_cb_service_arg: Optional[str] = None
+        self.send_cb_op_code_arg: Optional[str] = None
 
     def send_cb(self, tc_queue_entry: TcQueueEntryBase, com_if: ComInterface):
-        pass
+        self.send_cb_call_count += 1
+        self.send_cb_call_args = (tc_queue_entry, com_if)
 
     def queue_finished_cb(self, info: TcProcedureBase):
         pass
@@ -35,14 +41,27 @@ class TcHandlerMock(TcHandlerBase):
     def feed_cb(self, info: Optional[TcProcedureBase], wrapper: FeedWrapper):
         self.feed_cb_call_count += 1
         cast_wrapper = ProcedureCastWrapper(info)
+        self.send_cb_service_arg = None
+        self.send_cb_op_code_arg = None
         if info is not None:
-            if info.ptype != TcProcedureType.DEFAULT:
+            if info.ptype == TcProcedureType.DEFAULT:
+                self.feed_cb_def_proc_count += 1
                 def_info = cast_wrapper.to_def_procedure()
+
                 if def_info.service != "17":
                     self.is_feed_cb_valid = False
-                elif def_info.op_code != "0":
-                    self.is_feed_cb_valid = False
-            wrapper.queue_helper.add_pus_tc(PusTelecommand(service=17, subservice=1))
+                self.send_cb_service_arg = def_info.service
+                self.send_cb_op_code_arg = def_info.op_code
+                if def_info.service == "17":
+                    if def_info.op_code == "0":
+                        wrapper.queue_helper.add_pus_tc(PusTelecommand(service=17, subservice=1))
+                    elif def_info.op_code == "1":
+                        wrapper.queue_helper.add_pus_tc(PusTelecommand(service=17, subservice=1))
+                        wrapper.queue_helper.add_pus_tc(PusTelecommand(service=5, subservice=1))
+                    elif def_info.op_code == "2":
+                        wrapper.queue_helper.add_pus_tc(PusTelecommand(service=17, subservice=1))
+                        wrapper.queue_helper.add_wait(timedelta(milliseconds=20))
+                        wrapper.queue_helper.add_pus_tc(PusTelecommand(service=5, subservice=1))
 
 
 class TestBackend(TestCase):
@@ -58,7 +77,7 @@ class TestBackend(TestCase):
             tc_handler=self.tc_handler,
         )
 
-    def test_backend(self):
+    def test_idle(self):
         self.assertEqual(self.backend.tm_mode, TmMode.IDLE)
         self.assertEqual(self.backend.tc_mode, TcMode.IDLE)
         self.assertEqual(self.backend.com_if.get_id(), "dummy")
@@ -75,7 +94,7 @@ class TestBackend(TestCase):
         self.assertFalse(self.backend.com_if_active())
         self.assertFalse(self.com_if.is_open())
 
-    def test_com_if_start(self):
+    def test_basic_ops(self):
         self.backend.start()
         self.assertTrue(self.com_if.is_open())
         self.assertTrue(self.backend.com_if_active())
@@ -95,4 +114,21 @@ class TestBackend(TestCase):
             self.backend.periodic_op()
         self.backend.current_proc_info = DefaultProcedureInfo(service="17", op_code="0")
         res = self.backend.periodic_op()
+        # Only one queue entry which is handled immediately
         self.assertEqual(res.request, BackendRequest.TERMINATION_NO_ERROR)
+        self.assertEqual(self.tc_handler.feed_cb_def_proc_count, 1)
+        self.assertEqual(self.tc_handler.feed_cb_call_count, 1)
+        self.assertEqual(self.tc_handler.send_cb_call_count, 1)
+        self.assertIsNotNone(self.tc_handler.send_cb_call_args)
+        self.assertIsNotNone(self.tc_handler.send_cb_call_args[0])
+        self.assertIsNotNone(self.tc_handler.send_cb_call_args[1])
+        self.assertEqual(self.tc_handler.send_cb_call_args[1], self.com_if)
+        cast_wrapper = PacketCastWrapper(base=self.tc_handler.send_cb_call_args[0])
+        pus_entry = cast_wrapper.to_pus_tc_entry()
+        self.assertEqual(pus_entry.pus_tc, PusTelecommand(service=17, subservice=1))
+
+    def test_one_queue_multi_entry_ops(self):
+        self.backend.tm_mode = TmMode.IDLE
+        self.backend.tc_mode = TcMode.ONE_QUEUE
+        self.backend.current_proc_info = DefaultProcedureInfo(service="17", op_code="3")
+        res = self.backend.periodic_op()
