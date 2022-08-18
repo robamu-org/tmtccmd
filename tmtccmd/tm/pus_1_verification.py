b02ab670fa16b78fe521fc23255bb43b505fc427
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2022-08-18 16:17:41 +0200

    add deprecation dep and mark class as deprecated

--- tmtccmd/tm/pus_1_verification.py
+++ tmtccmd/tm/pus_1_verification.py
@@ -4,8 +4,10 @@ import struct
 from abc import abstractmethod
 from typing import Deque, Optional
 
+from deprecation import deprecated
+
 from spacepackets.ccsds.time import CdsShortTimestamp
-from spacepackets.ecss.tm import PusVersion, PusTelemetry
+from spacepackets.ecss.tm import PusTelemetry
 from spacepackets.ecss.pus_1_verification import (
     Service1Tm,
     Subservices,
@@ -19,6 +21,15 @@ from tmtccmd.logging import get_console_logger
 LOGGER = get_console_logger()
 
 
+class Service1FsfwWrapper:
+    def __init__(self, tm: Service1Tm):
+        self.tm = tm
+        if tm.has_failure_notice:
+            self.error_param_1 = struct.unpack("!I", tm.failure_notice.data[0:4])
+            self.error_param_2 = struct.unpack("!I", tm.failure_notice.data[4:8])
+
+
+@deprecated(deprecated_in="v3.0.0rc2", details="Use Service1FsfwWrapper instead")
 class Service1TmExtended(PusTmBase, PusTmInfoBase, Service1Tm):
     """Service 1 TM class representation. Can be used to deserialize raw service 1 packets.
     Only PUS C is supported.
