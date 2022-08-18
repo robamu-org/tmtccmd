022f7729b841006f8573e16f573051cc763786df
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2022-08-18 14:05:15 +0200

    API is a bit more elegant

--- tmtccmd/tc/decorator.py
+++ tmtccmd/tc/decorator.py
@@ -1,9 +1,19 @@
+import dataclasses
 import functools
 
+from tmtccmd.tc import TcHandlerBase
 from tmtccmd.tc.procedure import DefaultProcedureInfo
 from tmtccmd.tc.queue import DefaultPusQueueHelper
 
 SERVICE_HANDLER_DICT = dict()
+
+
+@dataclasses.dataclass
+class ServiceProviderParams:
+    handler_base: TcHandlerBase
+    info: DefaultProcedureInfo
+    queue_helper: DefaultPusQueueHelper
+    op_code: str
 
 
 def service_provider(service: str):
@@ -14,12 +24,8 @@ def service_provider(service: str):
         global SERVICE_HANDLER_DICT
 
         @functools.wraps(handler)
-        def service_handler_wrapper(
-            info: DefaultProcedureInfo,
-            queue_helper: DefaultPusQueueHelper,
-            op_code: str,
-        ):
-            return handler(info, queue_helper, op_code)
+        def service_handler_wrapper(p: ServiceProviderParams):
+            return handler(p)
 
         SERVICE_HANDLER_DICT.update({service: service_handler_wrapper})
         return service_handler_wrapper
@@ -28,9 +34,9 @@ def service_provider(service: str):
 
 
 def route_to_registered_service_handlers(
-    info: DefaultProcedureInfo, queue_helper: DefaultPusQueueHelper, op_code: str
+    service: str, p: ServiceProviderParams
 ) -> bool:
-    if info.service in SERVICE_HANDLER_DICT:
-        SERVICE_HANDLER_DICT[info.service](info, queue_helper, op_code)
+    if service in SERVICE_HANDLER_DICT:
+        SERVICE_HANDLER_DICT[service](p)
         return True
     return False
