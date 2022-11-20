7a5f3bacd3b910af94927c7e501d29cbb22a2a40
Robin Mueller <robin.mueller.m@gmail.com>
2022-11-20 17:53:16 +0100

    new release candidate

--- tmtccmd/__init__.py
+++ tmtccmd/__init__.py
@@ -33,7 +33,7 @@ VERSION_REVISION = 0
 
 # I think this needs to be in string representation to be parsed so we can't
 # use a formatted string here.
-__version__ = "3.0.0rc1"
+__version__ = "3.0.0rc2"
 
 
 LOGGER = get_console_logger()
