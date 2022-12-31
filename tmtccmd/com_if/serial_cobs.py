5e5be92950ead7131defc131a609aa2039a66982
Robin Mueller <robin.mueller.m@gmail.com>
2022-12-31 15:04:31 +0100

    impl all base classes

--- tmtccmd/com_if/serial_cobs.py
+++ tmtccmd/com_if/serial_cobs.py
@@ -1,6 +1,32 @@
 from tmtccmd.com_if import ComInterface
 from tmtccmd.com_if.serial_base import SerialComBase
+from tmtccmd.tm import TelemetryListT
 
 
 class SerialCobsComIF(SerialComBase, ComInterface):
-    pass
+    def __init__(self):
+        pass
+
+    def get_id(self) -> str:
+        pass
+
+    def initialize(self, args: any = None) -> any:
+        pass
+
+    def open(self, args: any = None) -> None:
+        pass
+
+    def is_open(self) -> bool:
+        pass
+
+    def close(self, args: any = None) -> None:
+        pass
+
+    def send(self, data: bytes):
+        pass
+
+    def receive(self, parameters: any = 0) -> TelemetryListT:
+        pass
+
+    def data_available(self, timeout: float, parameters: any) -> int:
+        pass
