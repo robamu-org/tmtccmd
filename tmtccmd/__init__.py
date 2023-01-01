a5b998a9a2f71c96d7670faa7de217fb79d7d35b
Robin Mueller <robin.mueller.m@gmail.com>
2023-01-01 19:15:16 +0100

    cant import this for some reason

--- tmtccmd/__init__.py
+++ tmtccmd/__init__.py
@@ -26,8 +26,15 @@ from tmtccmd.config.globals import set_default_globals_pre_args_parsing
 from tmtccmd.core import ModeWrapper
 from tmtccmd.tc import DefaultProcedureInfo, TcProcedureBase, ProcedureWrapper
 from tmtccmd.tc.handler import TcHandlerBase
-from .version import __version__
 
+
+VERSION_MAJOR = 4
+VERSION_MINOR = 0
+VERSION_REVISION = 0
+
+# I think this needs to be in string representation to be parsed so we can't
+# use a formatted string here.
+__version__ = "4.0.0a0"
 
 LOGGER = get_console_logger()
 
