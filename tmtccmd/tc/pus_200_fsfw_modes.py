60d6145ea3b6ffd28b076d68594282aa2424289e
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2023-01-16 14:51:34 +0100

    add pack_mode_command function

--- tmtccmd/tc/pus_200_fsfw_modes.py
+++ tmtccmd/tc/pus_200_fsfw_modes.py
@@ -3,6 +3,10 @@
 """
 import enum
 import struct
+from typing import Union
+
+from spacepackets.ecss import PusTelecommand
+from tmtccmd.pus import CustomPusServices
 from tmtccmd.pus.pus_200_fsfw_mode import Subservices
 
 
@@ -13,8 +17,20 @@ class Modes(enum.IntEnum):
     RAW = 3
 
 
-def pack_mode_data(object_id: bytes, mode: Modes, submode: int) -> bytearray:
-    """Mode 0: Off, Mode 1: Mode On, Mode 2: Mode Normal, Mode 3: Mode Raw"""
+def pack_mode_data(
+    object_id: bytes, mode: Union[Modes, int], submode: int
+) -> bytearray:
+    """FSFW modes: Mode 0: Off, Mode 1: Mode On, Mode 2: Mode Normal, Mode 3: Mode Raw"""
     mode_data = bytearray()
     mode_data += object_id + struct.pack("!I", mode) + struct.pack("B", submode)
     return mode_data
+
+
+def pack_mode_command(
+    object_id: bytes, mode: Union[int, Modes], submode: int
+) -> PusTelecommand:
+    return PusTelecommand(
+        service=CustomPusServices.SERVICE_200_MODE,
+        subservice=Subservices.TC_MODE_COMMAND,
+        app_data=pack_mode_data(object_id, mode, submode),
+    )
