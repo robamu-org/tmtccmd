31a7f9c1c0e241694a66509034bb27f621ebb6a5
Robin Mueller <robin.mueller.m@gmail.com>
2023-01-29 17:35:59 +0100

    only setup and teardown PTY/COBS interface once

--- tests/com/test_serial_cobs.py
+++ tests/com/test_serial_cobs.py
@@ -2,6 +2,7 @@ import os
 import sys
 import time
 import unittest
+from typing import Optional
 from unittest import TestCase
 
 from tmtccmd.com.serial_base import SerialCfg
@@ -10,31 +11,40 @@ from tmtccmd.com.serial_cobs import SerialCobsComIF
 
 @unittest.skipIf(sys.platform.startswith("win"), "pty only works on POSIX systems")
 class TestSerialCobsInterface(TestCase):
-    def setUp(self) -> None:
+    _COBS_IF: Optional[SerialCobsComIF] = None
+    _PTY_MASTER: Optional[int] = None
+
+    # The ComIF will spawn a separate receiver thread. Therefore, I'd like to have one
+    # instance for all the tests. This is done by using the class setup and teardown methods.
+    @classmethod
+    def setUpClass(cls) -> None:
         import pty
-        self.master, self.slave = pty.openpty()
-        sname = os.ttyname(self.slave)
-        self.ser_cfg = SerialCfg(
+        cls._PTY_MASTER, slave = pty.openpty()
+        sname = os.ttyname(slave)
+        ser_cfg = SerialCfg(
             com_if_id="pseudo_ser_cobs",
             serial_port=sname,
             baud_rate=9600,
             serial_timeout=1.0,
         )
-        self.cobs_if = SerialCobsComIF(self.ser_cfg)
-        self.cobs_if.open()
-        self.cobs_if.initialize()
+        cls._COBS_IF = SerialCobsComIF(ser_cfg)
+        cls._COBS_IF.open()
+        cls._COBS_IF.initialize()
+
+    def setUp(self) -> None:
+        pass
 
     def test_state(self):
-        self.assertTrue(self.cobs_if.is_open())
-        self.assertEqual(self.cobs_if.data_available(0), 0)
-        self.assertEqual(self.cobs_if.get_id(), "pseudo_ser_cobs")
+        self.assertTrue(self._COBS_IF.is_open())
+        self.assertEqual(self._COBS_IF.data_available(0), 0)
+        self.assertEqual(self._COBS_IF.get_id(), "pseudo_ser_cobs")
 
     def test_send(self):
         from cobs import cobs
         test_data = bytes([0x01, 0x02, 0x03])
         encoded_len = len(cobs.encode(test_data))
-        self.cobs_if.send(test_data)
-        encoded_packet = os.read(self.master, encoded_len + 2)
+        self._COBS_IF.send(test_data)
+        encoded_packet = os.read(self._PTY_MASTER, encoded_len + 2)
         test_data_read_back = cobs.decode(encoded_packet[1:-1])
         self.assertEqual(test_data_read_back, test_data)
 
@@ -46,14 +56,15 @@ class TestSerialCobsInterface(TestCase):
         full_data_to_send = bytearray([0x00])
         full_data_to_send.extend(encoded_test_data)
         full_data_to_send.append(0)
-        os.write(self.master, full_data_to_send)
+        os.write(self._PTY_MASTER, full_data_to_send)
         # Give the receiver thread some time to do its work.
         time.sleep(0.1)
-        self.assertEqual(self.cobs_if.data_available(0), 1)
-        packet_list = self.cobs_if.receive()
+        self.assertEqual(self._COBS_IF.data_available(0), 1)
+        packet_list = self._COBS_IF.receive()
         self.assertEqual(len(packet_list), 1)
         # Received data should be decoded now
         self.assertEqual(packet_list[0], test_data)
 
-    def tearDown(self) -> None:
-        self.cobs_if.close()
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls._COBS_IF.close()
