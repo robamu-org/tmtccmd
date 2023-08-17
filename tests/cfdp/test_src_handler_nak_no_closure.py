cdc61d5078a6ca42bb8c6c9bc65594adf5cdbb56
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2023-08-17 15:15:36 +0200

    finished test

--- tests/cfdp/test_src_handler_nak_no_closure.py
+++ tests/cfdp/test_src_handler_nak_no_closure.py
@@ -4,13 +4,15 @@ import sys
 
 from crcmod.predefined import PredefinedCrc
 
-from spacepackets.cfdp import CfdpLv
+from spacepackets.cfdp import CfdpLv, DirectiveType, ConditionCode
+from spacepackets.cfdp.pdu import FileDeliveryStatus, DeliveryCode
 from spacepackets.cfdp.tlv import ProxyPutRequest
 from spacepackets.util import ByteFieldU16, ByteFieldU8
-from tmtccmd.cfdp.defs import CfdpStates
+from tmtccmd.cfdp.defs import CfdpStates, TransactionId
 from tmtccmd.cfdp.handler import SourceHandler, FsmResult
 from tmtccmd.cfdp.handler.source import TransactionStep
 from tmtccmd.cfdp.request import PutRequestCfgWrapper, PutRequest
+from tmtccmd.cfdp.user import TransactionFinishedParams
 from .test_src_handler import TestCfdpSourceHandler
 
 
@@ -95,6 +97,7 @@ class TestCfdpSourceHandlerNoClosure(TestCfdpSourceHandler):
             CfdpLv.from_str("/tmp/source.txt"),
             CfdpLv.from_str("/tmp/dest.txt"),
         )
+        generic_tlv = proxy_op.to_generic_msg_to_user_tlv()
         put_req = PutRequest(
             destination_id=self.dest_id,
             source_file=None,
@@ -102,10 +105,41 @@ class TestCfdpSourceHandlerNoClosure(TestCfdpSourceHandler):
             # Let the transmission mode be auto-determined by the remote MIB
             trans_mode=None,
             closure_requested=None,
-            msgs_to_user=[proxy_op.to_generic_msg_to_user_tlv()],
+            msgs_to_user=[generic_tlv],
         )
         self.source_handler.put_request(put_req, self.remote_cfg)
         fsm_res = self.source_handler.state_machine()
+        self.assertTrue(fsm_res.pdu_holder.is_file_directive)
+        self.assertEqual(
+            fsm_res.pdu_holder.pdu_directive_type, DirectiveType.METADATA_PDU
+        )
+        metadata_pdu = fsm_res.pdu_holder.to_metadata_pdu()
+        self.assertIsNotNone(metadata_pdu.options)
+        self.assertEqual(len(metadata_pdu.options), 1)
+        self.assertEqual(metadata_pdu.options[0], generic_tlv)
+        self.assertIsNone(metadata_pdu.source_file_name)
+        self.assertIsNone(metadata_pdu.dest_file_name)
+        self.assertEqual(fsm_res.states.state, CfdpStates.BUSY_CLASS_1_NACKED)
+        self.assertEqual(fsm_res.states.step, TransactionStep.SENDING_METADATA)
+        expected_id = TransactionId(
+            metadata_pdu.source_entity_id, metadata_pdu.transaction_seq_num
+        )
+        self.cfdp_user.transaction_indication.assert_called_once_with(expected_id)
+
+        self.source_handler.confirm_packet_sent_advance_fsm()
+        # Now the state machine should be finished.
+        fsm_res = self.source_handler.state_machine()
+        finished_params = TransactionFinishedParams(
+            transaction_id=expected_id,
+            condition_code=ConditionCode.NO_ERROR,
+            file_status=FileDeliveryStatus.FILE_STATUS_UNREPORTED,
+            delivery_code=DeliveryCode.DATA_COMPLETE,
+        )
+        self.cfdp_user.transaction_finished_indication.assert_called_once_with(
+            finished_params
+        )
+        self.assertEqual(fsm_res.states.state, CfdpStates.IDLE)
+        self.assertEqual(fsm_res.states.step, TransactionStep.IDLE)
 
     def _second_file_segment_handling(self, source_handler: SourceHandler):
         source_handler.confirm_packet_sent_advance_fsm()
